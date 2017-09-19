8cd9b76f80ebcacfb5c07116be92b703f367ff8e
erika vidal <diann852@gmail.com>
2017-09-18 21:47:46 -0500

    cambiando còdigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
+var vendido=["rosmery","erika","flor"];
+var nombre=document.getElementById("reserva");
 var firstSeatLabel = 1;
 $(_=> {
+  console.log(localStorage.getItem("usuarios"));
     var name= $('#nombre');
     var $cart = $('#selected-seats'),
         $counter = $('#counter'),
@@ -19,12 +22,12 @@ $(_=> {
             seats: {
                 f: {
                     price   : 100,
-                    classes : 'first-class', //your custom CSS class
+                    classes : 'first-class',
                     category: 'First Class'
                 },
                 e: {
                     price   : 40,
-                    classes : 'economy-class', //your custom CSS class
+                    classes : 'economy-class',
                     category: 'Economy Class'
                 }
             },
@@ -43,16 +46,30 @@ $(_=> {
                 ]
             },
             click: function () {
+              //console.log(this.settings);
+              //console.log(this.settings.data.classes);
+              $("#myModal").css("display","block");
+              $("#enviar").on("click",function(){
+                  vendido.forEach(function(elemento){
+                    console.log("ente al for");
+                      console.log(vendido);
+                    if(elemento==nombre.value){
+                      console.log("encontrado");
+                      //vendido.push(name);
+                      //localStorage.setItem("usuarios", vendido);
+                    }
+                  });
+                });
+                $(this).attr("nombre", nombre.value);
+                console.log($(this).attr("nombre"));
                 if (this.status() == 'available') {
-                    if($('#name').val()!="") {
-                        $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
+                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
                             .attr('id', 'cart-item-' + this.settings.id)
                             .data('seatId', this.settings.id)
                             .appendTo($cart);
                         $counter.text(sc.find('selected').length + 1);
                         $total.text(recalculateTotal(sc) + this.data().price);
                         return 'selected';
-                    }
                 } else if (this.status() == 'selected') {
                     $counter.text(sc.find('selected').length-1);
                     $total.text(recalculateTotal(sc)-this.data().price);
@@ -63,29 +80,28 @@ $(_=> {
                 } else {
                     return this.style();
                 }
-            }
+              }
+        });
+
+    $(".close").on("click",function() {
+            $("#myModal").css("display","none");
         });
     $('#selected-seats').on('click', '.cancel-cart-item', function () {
         sc.get($(this).parents('li:first').data('seatId')).click();
     });
-    //let's pretend some seats have already been booked
     sc.get(['1_2', '4_1', '7_1', '7_2']).status('unavailable');
-
-    var allNames=[];
-    $('.checkout-button').on('click',(e)=>{
-        e.preventDefault();
-            allNames.push($("#nombre").val());
-            $(".status").removeClass("hide");
-            $('<p>La reservación fue exitosa!</p>').appendTo($(".status"));
-    })
 });
 
 function recalculateTotal(sc) {
     var total = 0;
-    //basically find every selected seat and sum its price
     sc.find('selected').each(function () {
         total += this.data().price;
     });
     return total;
 }
 
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
